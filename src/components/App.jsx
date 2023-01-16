722523fd52c88fbfaf34fed2455040cb15852915
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2023-01-16 14:29:31 +0200

    Friend

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import User from "./User/User";
 import {Statistics} from "./Statistics/Statistics";
+import {FriendList} from "./Friends/FriendList";
 
 import user from './Data/user.json';
 import data from './Data/data.json';
+import friends  from "./Data/friends.json";
 
 export const App = () => {
   return (
@@ -15,6 +17,7 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </div>
   );
 };
